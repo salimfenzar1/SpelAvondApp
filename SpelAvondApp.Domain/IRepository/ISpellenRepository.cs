d8ca1e76289955b49447a9832e8569d36b173d0d
salimfenzar <s.fenzar@student.avans.nl>
2024-10-25 16:54:27 +0200

    small code cleanup

--- SpelAvondApp.Domain/IRepository/ISpellenRepository.cs
+++ SpelAvondApp.Domain/IRepository/ISpellenRepository.cs
@@ -8,15 +8,15 @@ public interface ISpellenRepository
     Task AddBordspelAsync(Bordspel bordspel);
     Task UpdateBordspelAsync(Bordspel bordspel);
     Task DeleteBordspelAsync(int id);
-    Task<Bordspel> GetBordspelByIdAsync(int id);
+    Task<Bordspel?> GetBordspelByIdAsync(int id);
     Task<List<Bordspel>> GetAllBordspellenAsync();
 
     // BordspellenAvond Methods
     Task AddBordspellenAvondAsync(BordspellenAvond avond);
     Task UpdateBordspellenAvondAsync(BordspellenAvond avond);
     Task DeleteBordspellenAvondAsync(int id);
-    Task<BordspellenAvond> GetBordspellenAvondByIdAsync(int id);
-    Task<List<BordspellenAvond>> GetAllBordspellenAvondenAsync();
+    Task<BordspellenAvond?> GetBordspellenAvondByIdAsync(int id);
+    Task<List<BordspellenAvond?>> GetAllBordspellenAvondenAsync();
 
     Task<List<Bordspel>> GetBordspellenByIdsAsync(List<int> ids);
 
@@ -26,10 +26,10 @@ public interface ISpellenRepository
 
     Task<List<BordspellenAvond>> GetAvondenByOrganisatorAsync(string organisatorId);
 
-    Task<Inschrijving> GetInschrijvingAsync(string userId, int avondId);
+    Task<Inschrijving?> GetInschrijvingAsync(string userId, int avondId);
     Task<List<BordspellenAvond>> GetAvondenWaarIngeschrevenAsync(string userId);
 
-    Task<BordspellenAvond> GetAvondMetDieetOptiesAsync(int avondId);
+    Task<BordspellenAvond?> GetAvondMetDieetOptiesAsync(int avondId);
 
     Task<bool> HeeftInschrijvingOpDatumAsync(string userId, DateTime datum);
 
